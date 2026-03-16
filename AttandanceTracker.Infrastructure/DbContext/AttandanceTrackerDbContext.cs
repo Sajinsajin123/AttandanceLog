dc2f96fd42d4b9efd3abc40107511ab6b44f0d53
sajin s <sajins447@gmail.com>
2026-03-16 20:08:14 +0530

    Added Role API and validation

---
+++ AttandanceTracker.Infrastructure/DbContext/AttandanceTrackerDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AttandanceTracker.Infrastructure.DBContext
+{
+    public class AttandanceTrackerDbContext:DbContext
+    {
+        public AttandanceTrackerDbContext(DbContextOptions<AttandanceTrackerDbContext>option):base(option) { }
+    }
+}
